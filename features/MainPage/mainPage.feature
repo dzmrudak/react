9fefc76be0cafc5470d5067d66c0c697c5ffc027
dzmitry rudak <dzmitry.rudak@masternaut.com>
2023-07-17 14:36:31 +0200

    Add server on/close for wdio

--- features/MainPage/mainPage.feature
+++ features/MainPage/mainPage.feature
@@ -32,4 +32,3 @@ Feature: My First JS App
       | " "           |
       | "  "          |
       | "\u00A0"      |
-
--- features/MainPage/step-definitions/given.js
+++ features/MainPage/step-definitions/given.js
@@ -1,5 +1,20 @@
-import { Given } from '@wdio/cucumber-framework';
+import { BeforeAll, AfterAll, Given } from '@wdio/cucumber-framework';
 import goToUrl from '../support/actions/goToUrl';
+import {app, server} from '../../../server';
+
+BeforeAll(async () => {
+    await new Promise((resolve) => {
+      server.on('listening', resolve);
+    });
+  });
+  
+  AfterAll(async () => {
+    await new Promise((resolve) => {
+      server.close(resolve);
+    });
+  });
+  
+
 
 Given(/^A web browser is at the "(Main)" page$/, page => {
     goToUrl(page);
