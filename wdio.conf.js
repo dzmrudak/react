f659f9e9e955d8ea25d4c6c1821aa31050fa7ae6
dzmrudak <51719312+dzmrudak@users.noreply.github.com>
2023-07-17 14:45:32 +0200

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -122,7 +122,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [['selenium-standalone', {drivers: {chrome: '114.0.5735.16'}}]],
+    services: [['selenium-standalone', {drivers: {chrome: '113.0.5672.126'}}]],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
