1e93eeeff03f8056a0103654a6e4468d8def5c64
dzmitry rudak <dzmitry.rudak@masternaut.com>
2023-07-17 17:39:47 +0200

    Update wdio.conf.js

--- features/MainPage/support/assertions/assertPIcturesInGrid.js
+++ features/MainPage/support/assertions/assertPIcturesInGrid.js
@@ -1,5 +1,6 @@
 import mainPage from "../../pageobjects/main.page";
 
 export default async (numberOfPics) => {
-    await expect(await mainPage.picturesInGrid).toHaveLength(numberOfPics);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    expect(await mainPage.picturesInGrid).toHaveLength(numberOfPics);
 };
